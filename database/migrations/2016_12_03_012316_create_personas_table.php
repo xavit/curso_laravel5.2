6b57b9dddf421983ef4dff2a0a21ce0f58e2f19c
Javier Claros - CEGOS <javiclaros@gmail.com>
2016-12-03 02:40:32 +0100

    Agregando Migraciones y algunos configuraciones al proyecto

---
+++ database/migrations/2016_12_03_012316_create_personas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 150);
+            $table->string('last_name', 150);
+            $table->integer('ci');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
